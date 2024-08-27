78d5cf87d68256d0aa22cd8edc99fb132328b577
Juan Carlos Cotrina <juan.cotrina.m@uni.pe>
2024-08-27 19:52:17 +0000

    feat:component hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,29 @@
-// Placeholder to host a component
+import React, { useState, useEffect } from 'react';
+
+const Hours = () => {
+    const [hours, setHours] = useState('');
+  
+    useEffect(() => {
+      const today = new Date().getDay();
+      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+      const getHours = () => {
+        if (today >= 1 && today <= 5) {
+          return '10 a.m. to 4 p.m.';
+        } else {
+          return '9 a.m. to 8 p.m.';
+        }
+      };
+  
+      setHours(`${daysOfWeek[today]} ${getHours()}`);
+    }, []);
+  
+    return (
+      <div id="hours">
+        <h2>Today's hours</h2>
+        <p>{hours}</p>
+      </div>
+    );
+  };
+
+export default Hours;
